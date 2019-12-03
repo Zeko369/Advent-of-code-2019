997bd0189f08cef86f4087e260c1a60c6e68e0f8
Fran Zekan <zekan.fran369@gmail.com>
2019-12-03 22:05:38 +0100

    Add day 3

---
+++ 3/second.cpp
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <math.h>
+
+#define MAX_SIZE 20000
+#define ARRAY_SIZE 1000
+
+int n[2];
+int minSum = 0;
+int field[MAX_SIZE][MAX_SIZE];
+
+struct command {
+  char dir;
+  int value;
+};
+
+command arr[2][ARRAY_SIZE];
+
+void readData() {
+  for(int i = 0; i < 2; i++) {
+    char tmp = 'a';
+    int j = 0;
+
+    while(tmp != '\n') {
+      int tmpValue;
+      char tmpDir;
+      scanf("%c%d", &tmpDir, &tmpValue);
+      scanf("%c", &tmp);
+      arr[i][j++] = {tmpDir, tmpValue};
+    }
+
+    n[i] = j;
+  }
+}
+
+void print() {
+  for(int i = 0; i < MAX_SIZE; i++) {
+    for(int j = 0; j < MAX_SIZE; j++) {
+      printf("%d", field[i][j]);
+    }
+    printf("\n");
+  }
+}
+
+void move(char dir, int *x, int *y) {
+  switch (dir) {
+    case 'D':
+      (*y)++;
+      break;
+    case 'U':
+      (*y)--;
+      break;
+    case 'L':
+      (*x)--;
+      break;
+    case 'R':
+      (*x)++;
+      break;
+    default:
+      printf("Parsing error");
+      break;
+  }
+}
+
+int main() {
+  readData();
+
+  int x = MAX_SIZE / 2, y = MAX_SIZE / 2;
+  int pos = 1;
+  for(int i = 0; i < n[0]; i++) {
+    command c = arr[0][i];
+
+    for(int j = 0; j < c.value; j++) {
+      move(c.dir, &x, &y);
+      field[y][x] = pos++;
+    }
+  }
+
+  // print();
+
+  x = MAX_SIZE / 2;
+  y = MAX_SIZE / 2;
+  pos = 1;
+  for(int i = 0; i < n[1]; i++) {
+    command c = arr[1][i];
+
+    for(int j = 0; j < c.value; j++) {
+      move(c.dir, &x, &y);
+
+      if(field[y][x] > 0) {
+        int sum = field[y][x] + pos;
+        if(sum < minSum || minSum == 0) {
+          minSum = sum;
+        }
+      }
+
+      pos++;
+    }
+  }
+
+  printf("%d\n", minSum);
+
+  return 0;
+}
